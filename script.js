45a943af48e6ab4d92bab94fb2ddf83c158cadaf
kambleps <125243027+kambleps@users.noreply.github.com>
2023-04-13 15:50:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,17 +12,17 @@ class Animal{
 		return this.species;
 	}
 	makeSound(){
-		console.log("The "+this.species+" make sound.")
+		console.log("The " + this.species + " makes a sound.")
 	}
 }
 class Cat extends Animal{
 	purr(){
-		console.log("purr");
+		console.log("The Siamese makes a sound");
 	}
 }
 class Dog extends Animal{
 	bark(){
-		console.log("woof");
+		console.log("The Golden Retriever makes a sound");
 	}
 }
 
