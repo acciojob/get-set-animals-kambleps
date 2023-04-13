8797b47c9f21148ee6f982495665386f14fedad3
kambleps <125243027+kambleps@users.noreply.github.com>
2023-04-13 15:47:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 //complete this code
-class Animal {}
+//class Animal {}
 
-class Dog extends Animal {}
+//class Dog extends Animal {}
 
-class Cat extends Animal {}
+//class Cat extends Animal {}
+class Animal{
+	constructor(species){
+		this.species = species ;
+	}
+	get species(){
+		return this.species;
+	}
+	makeSound(){
+		console.log("The "+this.species+" make sound.")
+	}
+}
+class Cat extends Animal{
+	purr(){
+		console.log("purr");
+	}
+}
+class Dog extends Animal{
+	bark(){
+		console.log("woof");
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
